a4bdcfd873d333024fdcfde9543aa4727e73f57d
Martin Studna <martin.studna2@gmail.com>
2019-04-02 23:25:58 +0200

    init

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello() {
+    return "Hello world! ";
+}
+exports.hello = hello;
+console.log(hello());
+//# sourceMappingURL=index.js.map
